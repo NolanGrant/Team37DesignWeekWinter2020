729e08e992c637042c91beb93254ce65997b2a40
CorbenG <61744834+CorbenG@users.noreply.github.com>
2020-03-04 11:42:15 -0500

    Add mech sprites and start improvements
    Added the mech sprites
    Shrunk the size of the arms
    Started on score system

---
+++ DesignWeekWinter2020/Assets/EnemyScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScore : MonoBehaviour
+{
+    public float lifetime;
+    public float speed;
+    float timer;
+
+    public GameObject scorePop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        transform.Translate(transform.up * speed);
+
+        if(timer >= lifetime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
