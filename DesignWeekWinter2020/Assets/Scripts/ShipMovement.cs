4d254d342861940c864f860ab77a4a4c86a75e9d
CorbenG <61744834+CorbenG@users.noreply.github.com>
2020-03-05 11:52:00 -0500

    Fix Ship movement

--- DesignWeekWinter2020/Assets/Scripts/ShipMovement.cs
+++ DesignWeekWinter2020/Assets/Scripts/ShipMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
+    public float movement = 0;
+    public float speed;
+    Rigidbody2D _body;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _body.velocity = new Vector2(0, 0);
+        _body.velocity = transform.right * movement * speed;
+
+        movement = 0;
     }
 }
+
