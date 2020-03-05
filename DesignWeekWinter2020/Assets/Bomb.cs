23ac82f9f8f082024e3c8a87e2633e3a6407d14a
NolanGrant <41460773+NolanGrant@users.noreply.github.com>
2020-03-05 18:20:35 -0500

    Bombs added

---
+++ DesignWeekWinter2020/Assets/Bomb.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("RightWeapon") ||
+            collision.gameObject.layer == LayerMask.NameToLayer("LeftWeapon"))
+        {
+            collision.GetComponentInParent<ArmHealth>().currentHealth = 0;
+
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+            Destroy(gameObject);
+        }
+    }
+}
